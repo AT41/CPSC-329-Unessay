56211affd428892bfb92efc027137978008a975f
anthony.tang2@ucalgary.ca <anthony.tang2@ucalgary.ca>
2020-06-14 16:48:26 -0600

    Created password files, Completed common passwords attack, Implemented non-memoization Levenschtein distance

---
+++ src/commonPasswords/CommonPasswordsAttack.java
@@ -0,0 +1,110 @@
+package commonPasswords;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import types.AttackAPI;
+
+public class CommonPasswordsAttack extends AttackAPI {
+	private static final String allPasswords = "src/commonPasswords/passwordFiles";
+	private String plaintextPassword;
+	private int currentLevenschteinDistance = 10;
+	private String levenGuess = "";
+	
+	public static void main(String[] args) {
+		AttackAPI cpa = new CommonPasswordsAttack("awefweaf23aqf32awf");
+		cpa.calculateMetrics();
+	}
+	
+	public CommonPasswordsAttack(String plaintextPassword) {
+		this.plaintextPassword = plaintextPassword;
+	}
+	
+	@Override
+	public void updateMetrics() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void calculateMetrics() {
+		File[] files = new File(allPasswords).listFiles();
+		BufferedReader br;
+		String toCompare = "";
+		for (int i = 0; i < files.length; i++) {
+			System.out.println("Moved to new file: " + i);
+			try {
+				br = new BufferedReader(new FileReader(files[i]));
+			
+			} catch (FileNotFoundException e) {
+				System.err.println("File at index " + i + " wasn't found");
+				e.printStackTrace();
+				return;
+			}
+			try {
+				toCompare = br.readLine();
+				while (toCompare != null) {
+					int levenDist = calculateLevenschtein(this.plaintextPassword, toCompare);
+					if (currentLevenschteinDistance > levenDist) {
+						this.currentLevenschteinDistance =  levenDist;
+						this.levenGuess = toCompare;
+					}
+					
+					if (toCompare.equals(this.plaintextPassword)) {
+						System.out.println("Got a match!");
+						return;
+					}
+					
+					toCompare = br.readLine();
+				}
+			} catch (IOException e) {
+				System.err.println("Could not read line");
+				e.printStackTrace();
+				return;
+			}
+		}
+
+		System.out.println(this.currentLevenschteinDistance);
+		System.out.println(this.levenGuess);
+	}
+
+	@Override
+	public void makeGuess() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkGuess() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private int calculateLevenschtein(String s1, String s2) {
+		if (s1 == null) {
+			return s2.length();
+		} else {
+			int[][] memo = new int[s1.length() - 1][s2.length() - 1];
+			return levenschtein(s1, s2, 0, 0, 0);
+		}
+	}
+	
+	// TODO ANTHONY Consider memoization if too slow
+	private int levenschtein(String s1, String s2, int length1, int length2, int currentDistance) {
+		if (currentDistance > currentLevenschteinDistance) {
+			return Integer.MAX_VALUE;
+		}
+		else if (s1.length() == length1 || s2.length() == length2) {
+			int temp = Math.max(s1.length() - length1, s2.length() - length2) + currentDistance;
+			return temp > currentLevenschteinDistance? Integer.MAX_VALUE : temp;
+		} else {
+			int lev1 = (s1.charAt(length1) == s2.charAt(length2)) ? levenschtein(s1, s2, length1 + 1, length2 + 1, currentDistance) : levenschtein(s1, s2, length1 + 1, length2 + 1, currentDistance + 1);
+			int lev2 = levenschtein(s1, s2, length1 + 1, length2, currentDistance + 1);
+			int lev3 = levenschtein(s1, s2, length1, length2 + 1, currentDistance + 1);
+			return Math.min(Math.min(lev1, lev2), lev3);
+		}
+	}
+}
