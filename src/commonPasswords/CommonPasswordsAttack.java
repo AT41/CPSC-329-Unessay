977dadf68aacc6bf6a4ce0b78a325b092c4ffce6
anthony.tang2@ucalgary.ca <anthony.tang2@ucalgary.ca>
2020-06-14 21:20:28 -0600

    Levenschtein solved through Dynamic Programming

--- src/commonPasswords/CommonPasswordsAttack.java
+++ src/commonPasswords/CommonPasswordsAttack.java
@@ -11,11 +11,11 @@ import types.AttackAPI;
 public class CommonPasswordsAttack extends AttackAPI {
 	private static final String allPasswords = "src/commonPasswords/passwordFiles";
 	private String plaintextPassword;
-	private int currentLevenschteinDistance = 10;
+	private int currentLevenschteinDistance = Integer.MAX_VALUE;
 	private String levenGuess = "";
 	
 	public static void main(String[] args) {
-		AttackAPI cpa = new CommonPasswordsAttack("awefweaf23aqf32awf");
+		AttackAPI cpa = new CommonPasswordsAttack("fwefw");
 		cpa.calculateMetrics();
 	}
 	
@@ -55,6 +55,8 @@ public class CommonPasswordsAttack extends AttackAPI {
 					
 					if (toCompare.equals(this.plaintextPassword)) {
 						System.out.println("Got a match!");
+						System.out.println(this.currentLevenschteinDistance);
+						System.out.println(this.levenGuess);
 						return;
 					}
 					
@@ -69,6 +71,8 @@ public class CommonPasswordsAttack extends AttackAPI {
 
 		System.out.println(this.currentLevenschteinDistance);
 		System.out.println(this.levenGuess);
+		//System.out.println(this.calculateLevenschtein("123456", "aw23"));
+		//System.out.println(this.calculateLevenschtein("123456", "awefweaf23aqf3"));
 	}
 
 	@Override
@@ -87,24 +91,33 @@ public class CommonPasswordsAttack extends AttackAPI {
 		if (s1 == null) {
 			return s2.length();
 		} else {
-			int[][] memo = new int[s1.length() - 1][s2.length() - 1];
-			return levenschtein(s1, s2, 0, 0, 0);
+			int[][] memo = new int[s1.length()+1][s2.length()+1];
+			for (int i = 0; i <= s1.length(); i++) {
+				for (int j = 0; j <= s2.length(); j++) {
+					memo[i][j] = -1;
+				}
+			}
+			return levenschtein(s1, s2, 0, 0, memo);
 		}
 	}
 	
 	// TODO ANTHONY Consider memoization if too slow
-	private int levenschtein(String s1, String s2, int length1, int length2, int currentDistance) {
-		if (currentDistance > currentLevenschteinDistance) {
-			return Integer.MAX_VALUE;
-		}
-		else if (s1.length() == length1 || s2.length() == length2) {
-			int temp = Math.max(s1.length() - length1, s2.length() - length2) + currentDistance;
-			return temp > currentLevenschteinDistance? Integer.MAX_VALUE : temp;
+	private int levenschtein(String s1, String s2, int length1, int length2, int[][] memo) {
+		if (memo[length1][length2] != -1) {
+			int temp = memo[length1][length2];
+			return /*temp >= currentLevenschteinDistance ? Integer.MAX_VALUE / 2 :*/ temp;
+		} else if (s1.length() == length1 || s2.length() == length2) {
+			int temp = Math.max(s1.length() - length1, s2.length() - length2);
+			int ans = /*temp >= currentLevenschteinDistance? Integer.MAX_VALUE / 2:*/ temp;
+			memo[length1][length2] = ans;
+			return ans;
 		} else {
-			int lev1 = (s1.charAt(length1) == s2.charAt(length2)) ? levenschtein(s1, s2, length1 + 1, length2 + 1, currentDistance) : levenschtein(s1, s2, length1 + 1, length2 + 1, currentDistance + 1);
-			int lev2 = levenschtein(s1, s2, length1 + 1, length2, currentDistance + 1);
-			int lev3 = levenschtein(s1, s2, length1, length2 + 1, currentDistance + 1);
-			return Math.min(Math.min(lev1, lev2), lev3);
+			int lev1 = (s1.charAt(length1) == s2.charAt(length2)) ? levenschtein(s1, s2, length1 + 1, length2 + 1, memo) : levenschtein(s1, s2, length1 + 1, length2 + 1, memo) + 1;
+			int lev2 = levenschtein(s1, s2, length1 + 1, length2, memo) + 1;
+			int lev3 = levenschtein(s1, s2, length1, length2 + 1, memo) + 1;
+			int ans = Math.min(Math.min(lev1, lev2), lev3);
+			memo[length1][length2] = ans;
+			return ans;
 		}
 	}
 }
